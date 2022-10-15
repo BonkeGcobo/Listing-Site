21c733c405df5b86ba651730c6ee4235e505b5f4
bonkegcobo <madixmicro@gmail.com>
2022-10-15 04:17:03 -0700

    Create header

---
+++ src/Home/Header.js
@@ -0,0 +1,8 @@
+import React from "react"
+import './header.css'
+const Header = () =>(
+    <div className="header">
+     <button>List your site</button>
+    </div>   
+);
+export default Header
---
+++ src/Home/Search.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Header'
+
+const Search = () =>(
+    <div>Search</div>
+);
+export default Search
